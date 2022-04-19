1fc1b97c6e2aeb909a5c7398d79e846dfc1182c3
omar-othmann <34454535+omar-othmann@users.noreply.github.com>
2022-04-19 16:29:56 +0200

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@
 class Config:
     ip = 'localhost'  # keep it localhost
     user = 'root'  # mysql user
-    password = '123456omar123456'  # mysql database.
+    password = 'password'  # mysql database.
     select = 'QBCoreFramework_108085'  # qb-core database
     ownerId = 428566835797950466  # this is owner of bot, you should get your discord id and past it here.
     logChannelId = 964526014996832296  # get your log channel id from your discord server, right click on it then copy id
